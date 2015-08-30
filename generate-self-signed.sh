99b0cfc21202cb89711065749859c997323bee8d
Thomas Maddox <thomasem@users.noreply.github.com>
2015-08-30 16:17:30 -0500

    Update generate-self-signed.sh

--- generate-self-signed.sh
+++ generate-self-signed.sh
@@ -9,13 +9,17 @@ SS_O=${SS_O:-None}
 SS_L=${SS_L:-"San Antonio"}
 SS_OU=${SS_OU:-IT}
 
-SUBJ="C=$SS_C/ST=$SS_ST/O=$SS_O/L=$SS_L/CN=$SS_CN/OU=$SS_OU"
+SS_KEY=${SS_KEY:-ssl}
+SS_CSR=${SS_CSR:-ssl}
+SS_CERT=${SS_CERT:-ssl}
+
+SUBJ="/C=$SS_C/ST=$SS_ST/O=$SS_O/L=$SS_L/CN=$SS_CN/OU=$SS_OU"
 PASS_ARGS=""
 if [ -z "$SS_PASS" ]; then
   PASS_ARGS="-passin pass:$SS_PASS"
 fi
 
 sudo mkdir -p "$SS_DIR"
-sudo openssl genrsa -out "$SS_DIR/priv.key" 2048
-sudo openssl req -new -subj "$SUBJ" -key "$SS_DIR/priv.key" -out "$SS_DIR/ssl.csr" $PASS_ARGS
-sudo openssl x509 -req -days 365 -in "$SS_DIR/ssl.csr" -signkey "$SS_DIR/priv.key" -out "$SS_DIR/ssl.crt"
+sudo openssl genrsa -out "$SS_DIR/$SS_KEY.key" 2048
+sudo openssl req -new -subj "$SUBJ" -key "$SS_DIR/$SS_KEY.key" -out "$SS_DIR/$SS_CSR.csr" $PASS_ARGS
+sudo openssl x509 -req -days 365 -in "$SS_DIR/$SS_CSR.csr" -signkey "$SS_DIR/$SS_KEY.key" -out "$SS_DIR/$SS_CERT.crt"
