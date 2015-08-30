a8305ba308620d99018e325203d19e5b2d227062
Thomas Maddox <thomasem@users.noreply.github.com>
2015-08-30 16:06:59 -0500

    Create generate-self-signed.sh

---
+++ generate-self-signed.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# SS_PASS defaults to undefined
+SS_DIR=${SS_DIR:-$(pwd)}
+SS_CN=${SS_CN:-$(hostname)}
+SS_C=${SS_CO:-US}
+SS_ST=${SS_ST:-TX}
+SS_O=${SS_O:-IT}
+SS_L=${SS_L:-"San Antonio"}
+SS_OU=${SS_OU:-IT}
+
+SUBJ="C=$SS_C/ST=$SS_ST/O=$SS_O/L=$SS_L/CN=$SS_CN/OU=$SS_OU"
+PASS_ARGS=""
+if [ -z "$SS_PASS" ]; then
+  PASS_ARGS="-passin pass:$SS_PASS"
+fi
+
+sudo mkdir -p "$SS_DIR"
+sudo openssl genrsa -out "$SS_DIR/priv.key" 2048
+sudo openssl req -new -subj "$SUBJ" -key "$SS_DIR/priv.key" -out "$SS_DIR/ssl.csr" $PASS_ARGS
+sudo openssl x509 -req -days 365 -in "$SS_DIR/ssl.csr" -signkey "$SS_DIR/priv.key" -out "$SS_DIR/ssl.crt"
