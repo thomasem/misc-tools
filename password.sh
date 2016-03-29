1627ce96cb18982075d76a15cf02558e47ed1bd4
Thomas Maddox <thomasem@users.noreply.github.com>
2016-03-29 09:59:09 -0500

    Update password.sh

--- password.sh
+++ password.sh
@@ -3,5 +3,5 @@
 # Place at /bin/password or somewhere on your environment PATH, give execute permissions and use inline like:
 # `$ cmd -u username -p $(password)`
 
-read -s -p "Enter Password: " pass
+read -s -p "Enter Secret: " pass
 echo $pass
